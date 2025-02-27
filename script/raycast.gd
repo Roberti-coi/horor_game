8b248f3d8fff3080bb2c769846625d130f023b02
Roberti-coi <banjoenzll@gmail.com>
2025-02-27 19:06:00 +0300

    Add files via upload

--- script/raycast.gd
+++ script/raycast.gd
@@ -1,18 +1,25 @@
 extends RayCast3D
 
-var int_text
+var int_text  # Переменная для хранения ссылки на текст взаимодействия
 
 func _ready():
+	# Получаем ссылку  взаимодействия 
 	int_text = get_node("/root/" + get_tree().current_scene.name + "/UI/interact_text")
+	int_text.visible = false  # Скрываем текст взаимодействия при старте
 
 func _process(_delta):
+	
 	if is_colliding():
-		var hit = get_collider()
+		var hit = get_collider()  
+		
+		# Проверяем, имеет ли объект метод взаимодействия
 		if hit.has_method("interact"):
-			int_text.visible = true
-		if Input.is_action_just_pressed("interact"):
-			hit.interact()
+			int_text.visible = true 
+			
+			# Если игрок нажал кнопку взаимодействия
+			if Input.is_action_just_pressed("interact"):
+				hit.interact()  
 		else:
-			int_text.visible = false
+			int_text.visible = false 
 	else:
-		int_text.visible = false
+		int_text.visible = false 
