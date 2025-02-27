2216e61ee99a29f2336ac67a66c93742fa3dee45
Roberti-coi <banjoenzll@gmail.com>
2025-02-27 11:24:04 +0300

    Add files via upload

--- script/flashlight.gd
+++ script/flashlight.gd
@@ -3,7 +3,7 @@ extends SpotLight3D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("flashlight"):
 		visible = !visible
 		$toggle.play()
