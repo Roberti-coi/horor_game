62d8f97ad6c0e7b5f00b91512bcc5481bbfd154f
Roberti-coi <banjoenzll@gmail.com>
2025-02-28 12:26:11 +0300

    Add files via upload

--- script/timer.gd
+++ script/timer.gd
@@ -1,3 +1,22 @@
+#extends Label
+#
+#var time = 0
+#var timer_on = false
+#
+#func _ready() -> void:
+	#timer_on = true  
+#
+#func _process(delta):
+	#if timer_on:
+		#time += delta * 3
+		#var mils = int(fmod(time, 1) * 1000)  # Приводим к целому числу
+		#var secs = int(fmod(time, 60))
+		#var mins = int(fmod(time / 60, 60))
+		#var hr = int(fmod(time / 3600, 24))
+		#var dy = int(fmod(time / 86400, 30)) 
+#
+		#var time_passed = "%02d : %02d : %02d : %02d : %03d" % [dy, hr, mins, secs, mils]
+		#text = time_passed  # Обновляем текст метки
 extends Label
 
 var time = 0
@@ -8,13 +27,11 @@ func _ready() -> void:
 
 func _process(delta):
 	if timer_on:
-		time += delta * 3
-		var mils = int(fmod(time, 1) * 1000)  # Приводим к целому числу
-		var secs = int(fmod(time, 60))
-		var mins = int(fmod(time / 60, 60))
-		var hr = int(fmod(time / 3600, 24))
-		var dy = int(fmod(time / 86400, 30)) 
+		time += delta * 3  # Увеличиваем игровое время
+		var secs = int(fmod(time, 60))  # Секунды
+		var mins = int(fmod(time / 60, 60))  # Минуты
+		var hr = int(fmod(time / 3600, 24))  # Часы
 
-		var time_passed = "%02d : %02d : %02d : %02d : %03d" % [dy, hr, mins, secs, mils]
+		# Форматируем строку без дней и миллисекунд
+		var time_passed = "%02d : %02d : %02d" % [hr, mins, secs]
 		text = time_passed  # Обновляем текст метки
-#
